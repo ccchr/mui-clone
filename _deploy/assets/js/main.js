90ed122bb4e694e0e23a4ea29a8f4d7d6234bfe8
TJ Kohli <hello@tjkohli.com>
2017-05-29 22:27:58 -0400

    Started implementing framework

---
+++ _deploy/assets/js/main.js
@@ -0,0 +1,33 @@
+/*!
+ * 
+ * Copyright (c) 2017  All rights reserved.
+ * --------------------------------------------------------
+ * Made by Monogram. http://monogram.design/
+ */
+
+
+$(function(){
+	// Link smooth scrolling
+
+$(document).on('click', 'a.scroller', function(event) {
+	event.preventDefault();
+	var link = $(this).attr('href');
+	//or to scroll to the element with the ID "#someID":
+	TweenLite.to(window, 1.5, {scrollTo: link, ease: Power3.easeInOut });
+
+});
+
+// $(window).on('load scroll resize', function() {
+// 	var pageY = window.scrollY;
+//
+// 	$('.parallax:in-viewport').each(function() {
+// 		var $this = $(this);
+// 		var image = $this.find('.bg');
+// 		var formula = (pageY - $this.position().top) / 4
+//
+// 		TweenLite.to(image, 0, {
+// 			y: formula
+// 		});
+// 	});
+// });
+});
